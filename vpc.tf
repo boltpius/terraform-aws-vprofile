94f902b8ed92662fbfe194f6704fd12450d35242
boltpius <boltpius1@gmail.com>
2024-02-08 00:46:54 +0100

    added vpc

---
+++ vpc.tf
@@ -0,0 +1,19 @@
+module vpc {
+  source  = "terraform-aws-modules/vpc/aws//examples/complete"
+  version = "version"
+  name = var.vpc_name
+  cidr = var.vpc_cidr
+
+  azs = [var.avaiablity_zone1, var.avaiablity_zone2, var.avaiablity_zone3]
+  private_subnets = [var.priv1_cidr, var.priv2_cidr, var.priv3_cidr]
+  public_subnets = [var.pub1_cidr, var.pub2_cidr, var.pub3_cidr]
+
+  enable_nat_gatway = true
+  single-nat_gateway = true
+  enable_dns_hostnames = true 
+  enable_dns_support = true 
+  tags = {
+    Terraform = "true"
+    Environment = "Prod"
+  }
+}
