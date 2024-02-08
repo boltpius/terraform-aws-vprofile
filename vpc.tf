badf058735e0c0d2e6d410448cae7ddd41be9436
Pius <127052041+boltpius@users.noreply.github.com>
2024-02-08 01:54:36 +0100

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,15 +1,16 @@
 module vpc {
-  source  = "terraform-aws-modules/vpc/aws//examples/complete"
- # version = "version"
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.5.1"
   name = var.vpc_name
   cidr = var.vpc_cidr
+
 
   azs = [var.avaiablity_zone1, var.avaiablity_zone2, var.avaiablity_zone3]
   private_subnets = [var.priv1_cidr, var.priv2_cidr, var.priv3_cidr]
   public_subnets = [var.pub1_cidr, var.pub2_cidr, var.pub3_cidr]
 
-  enable_nat_gatway = true
-  single-nat_gateway = true
+  enable_nat_gateway = true
+  single_nat_gateway = true
   enable_dns_hostnames = true 
   enable_dns_support = true 
   tags = {
